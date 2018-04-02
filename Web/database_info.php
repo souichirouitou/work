0d084d11a3293ca98f90390d113df30dfdfdc878
伊藤颯一郎 <soichiroito@itousouichirous-MacBook-Pro.local>
2018-04-02 22:04:34 +0900

    Web

--- Web/database_info.php
+++ Web/database_info.php
@@ -1,11 +1,11 @@
 <?php
-$db_schedule['host'] = "localhost"; //DBサーバのURL
-$db_schedule['user'] = "schedule"; // ユーザ名
-$db_schedule['pass'] = "Schedule12()?A"; // 上記ユーザのパスワード
-$db_schedule['dbname'] = "scheduleManagement"; // データベース名
+$db_schedule['host'] = "xxx"; //DBサーバのURL
+$db_schedule['user'] = "xxx"; // ユーザ名
+$db_schedule['pass'] = "xxx"; // 上記ユーザのパスワード
+$db_schedule['dbname'] = "xxx"; // データベース名
 
-$db_login['host'] = "localhost"; //DBサーバのURL
-$db_login['user'] = "login"; // ユーザ名
-$db_login['pass'] = "Login12()?A"; // 上記ユーザのパスワード
-$db_login['dbname'] = "loginManagement"; // データベース名
+$db_login['host'] = "xxx"; //DBサーバのURL
+$db_login['user'] = "xxx"; // ユーザ名
+$db_login['pass'] = "xxx"; // 上記ユーザのパスワード
+$db_login['dbname'] = "xxx"; // データベース名
 ?>
