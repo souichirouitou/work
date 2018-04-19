596479e302931c04c57cc0949065894aab6d9205
伊藤颯一郎 <soichiroito@itousouichirous-MacBook-Pro.local>
2018-04-19 16:52:19 +0900

    work

--- web/Schedule/escape.php
+++
@@ -1,9 +0,0 @@
-<?php
-function escape($data){
-  if (is_array($data)) {//データが配列の場合
-    return array_map("escape",$data);
-  } else {//データが配列ではない場合
-    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8', false);
-  }
-}
-?>
--- web/Schedule/js/function.js
+++
@@ -1,8 +0,0 @@
-function ChangeTab(tabname) {
-   // タブメニュー実装
-   document.getElementById('tab1').style.display = 'none';
-   document.getElementById('tab2').style.display = 'none';
-   document.getElementById('tab3').style.display = 'none';
-   // タブメニュー実装
-   document.getElementById(tabname).style.display = 'block';
-}
