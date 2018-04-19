574b652d932d28039947e7602d8fd01b7a357c0b
伊藤颯一郎 <soichiroito@itousouichirous-MacBook-Pro.local>
2018-04-19 16:39:15 +0900

    ftp

---
+++ web/Schedule/database_info.php
@@ -0,0 +1,11 @@
+<?php
+$db_schedule['host'] = "xxx"; //DBサーバのURL
+$db_schedule['user'] = "xxx"; // ユーザ名
+$db_schedule['pass'] = "xxx"; // 上記ユーザのパスワード
+$db_schedule['dbname'] = "xxx"; // データベース名
+
+$db_login['host'] = "xxx"; //DBサーバのURL
+$db_login['user'] = "xxx"; // ユーザ名
+$db_login['pass'] = "xxx"; // 上記ユーザのパスワード
+$db_login['dbname'] = "xxx"; // データベース名
+?>
